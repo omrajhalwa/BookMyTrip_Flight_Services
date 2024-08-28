28a1be195924cd04f4d6bc777d4fa4ccbfaa3a63
omrajhalwa <omrajhalwa@gmail.com>
2024-08-28 16:39:18 +0530

    added validate middleware in city

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const {CityController} = require('../../controllers');
 
+const {CityMiddleware} = require('../../middlewares')
 
 const router=express.Router();
 
 
 // /api/v1/city POST
 router.post('/',
-    
+    CityMiddleware.validateCreateRequest,
     CityController.createCity);
 
 
