f42a8e273705e804a3e42e98cf5f720f9e8fc47d
omrajhalwa <omrajhalwa@gmail.com>
2024-08-28 15:58:28 +0530

    create city api

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -3,6 +3,12 @@ const { AirplaneService } = require('../services');
 const { SuccessResponse, ErrorResponse } = require('../utils/common');
 
 
+
+/**
+ * 
+ * POST :/airplanes
+ * req-body {modelNumber:'airbus234',capacity:200}
+ */
 async function createAirplane(req, res) {
     try {
         const airplane = await AirplaneService.createAirplane({
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,8 +4,13 @@ const { InfoController } = require('../../controllers');
 
 const airplaneRoutes=require('./airplane-routes');
 
+const cityRoutes=require('./city-routes');
+
 const router = express.Router();
 router.use('/airplane',airplaneRoutes);
+
+router.use('/city',cityRoutes);
+
 router.get('/info', InfoController.info);
 
 module.exports = router;
