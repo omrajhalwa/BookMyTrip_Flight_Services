0e2b516208bcb79878040ee5fbaf4c94d3973490
omrajhalwa <omrajhalwa@gmail.com>
2024-08-28 13:45:40 +0530

    added get all airplanes api

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -9,61 +9,46 @@ class CrudRepository{
     }
 
     async create(data){
-        try {
+     
             const response = await this.model.create(data);
             return response;
-        } catch (error) {
-            Logger.error('something went wrong in the crud repo : Create');
-            throw error;   
-        }
+    
     }
 
     async destroy(data){
-        try {
+        
             const response = await this.model.destroy({
                 where:{
                     id:data
                 }
             });
             return response;
-        } catch (error) {
-            Logger.error('something went wrong in the crud repo : Create');
-            throw error;   
-        }
+      
     }
 
     async get(data){
-        try {
+        
             const response = await this.model.findByPk(data);
             return response;
-        } catch (error) {
-            Logger.error('something went wrong in the crud repo : Create');
-            throw error;   
-        }
+       
     }
 
     async getAll(){
-        try {
+     
             const response = await this.model.findAll();
             return response;
-        } catch (error) {
-            Logger.error('something went wrong in the crud repo : Create');
-            throw error;   
-        }
+      
     }
 
     async update(id,data){
-        try {
+    
             const response = await this.model.update(data,{
                 where:{
                     id: id
                 }
             });
             return response;
-        } catch (error) {
-            Logger.error('something went wrong in the crud repo : Create');
-            throw error;   
-        }
+       
     }
 }
 
--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -12,5 +12,6 @@ router.post('/',
     AirplaneMiddleware.validateCreateRequest,
     AirplaneController.createAirplane);
 
+router.get('/',AirplaneController.getAirplanes);
 
 module.exports= router;
