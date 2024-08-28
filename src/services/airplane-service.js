b901213966586b1e215e7f0dcaaa483d411ce94f
omrajhalwa <omrajhalwa@gmail.com>
2024-08-28 15:13:58 +0530

    added delete airplane api

--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -46,8 +46,22 @@ async function getAirplane(id){
     }
 }
 
+async function destroyAirplane(id){
+    try {
+        const airplanes=await airplaneRepository.destroy(id);
+        return airplanes; 
+    } catch (error) {
+
+        if(error.statusCode === StatusCodes.NOT_FOUND){
+            throw new AppError('The airplane you requested to delete is not present', error.statusCode);
+        }
+        throw new AppError('Cannot fetch data of all the airplanes',StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
 module.exports={
     createAirplane,
     getAirplanes,
-    getAirplane
+    getAirplane,
+    destroyAirplane
 }
