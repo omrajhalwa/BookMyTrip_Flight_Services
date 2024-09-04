98b9f3be2607f75cd0559aa22631721d687ba47a
omrajhalwa <omrajhalwa@gmail.com>
2024-09-04 13:31:31 +0530

    FEAT: Added transaction to update seats

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -3,7 +3,7 @@ const airplane = require('../models/airplane');
 const CrudRepository = require('./crud-repository');
 const { Flight } = require('../models')
 const db = require('../models');
-const {addRowLockOnFlights} = require('../repositories/queries')
+const { addRowLockOnFlights } = require('../repositories/queries')
 
 class FlightRepository extends CrudRepository {
     constructor() {
@@ -50,31 +50,46 @@ class FlightRepository extends CrudRepository {
 
 
     async updateRemainingSeats(flightId, seats, dec = true) {
-           // Row Lock
-        await db.sequelize.query(addRowLockOnFlights(flightId))
-       
-        const flight = await Flight.findOne({
-            where: { id: flightId },
-            attributes: [
-                'id','flightNumber','airplaneId','departureAirportId',
-                'arrivalAirportId','arrivalTime','departureTime','price',
-                'boardingGate','totalSeats','createdAt','updatedAt',
-                // Make sure no extra spaces or empty attributes are here
-            ]
-        });
+
+        const transaction = await db.sequelize.transaction();
+
+        try {
+
+            // Row Lock
+            await db.sequelize.query(addRowLockOnFlights(flightId))
+
+            const flight = await Flight.findOne({
+                where: { id: flightId },
+                attributes: [
+                    'id', 'flightNumber', 'airplaneId', 'departureAirportId',
+                    'arrivalAirportId', 'arrivalTime', 'departureTime', 'price',
+                    'boardingGate', 'totalSeats', 'createdAt', 'updatedAt',
+                    // Make sure no extra spaces or empty attributes are here
+                ]
+            });
 
 
 
-        if ((+dec)) {
-            await flight.decrement('totalSeats', { by: seats });
+            if ((+dec)) {
+                await flight.decrement('totalSeats', { by: seats },{transaction:transaction});
 
-        } else {
-            await flight.increment('totalSeats', { by: seats });
+            } else {
+                await flight.increment('totalSeats', { by: seats },{transaction:transaction});
+
+            }
+            await flight.save();
+            
+            await transaction.commit();
+            return flight;
+
+        } catch (error) {
+            
+            await transaction.rollback();
+            throw error;
 
         }
-        await flight.save();
 
-        return flight;
+
     }
 }
 
