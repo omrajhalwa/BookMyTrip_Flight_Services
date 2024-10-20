ad989188bc1243df3f7bd026f8e1bf8f353868a4
omrajhalwa <omrajhalwa@gmail.com>
2024-10-21 00:48:13 +0530

    Added database port of aiven

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -14,7 +14,7 @@ config[env].username = process.env.DB_USERNAME || config[env].username;
 config[env].password = process.env.DB_PASSWORD || config[env].password;
 config[env].database = process.env.DB_NAME || config[env].database;
 config[env].host = process.env.DB_HOST || config[env].host;
-
+console.log(config[env]);
 module.exports = config[env];
 
 module.exports = {
